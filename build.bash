ed4e67d48de444ba4a24597226b02b46583b3154
PJ568 <PJ568@PJ568.eu.org>
2024-11-06 15:07:23 +0800

    【新增】优化部署流程

--- build.bash
+++ build.bash
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+workdir=$(pwd)
+
 BRANCHES=$(git branch -r | grep -v '\->' | sed 's|origin/||' | grep -v -E 'class' | tr '\n' ' ')
 
-mkdir -p build
+mkdir -p $workdir/build
 
-cp -v -u -r ./home/* ./build/
+cp -v -u -r $workdir/home/* $workdir/build/
 
 for branch in $BRANCHES; do
   git fetch origin $branch
   git checkout $branch
   echo "切换到分支：$branch"
+  python $workdir/scripts/replace_site_url.py $workdir/book.toml $branch
   mdbook clean && mdbook build
-  cp -v -u -r ./book/ ./build/$branch/
-  ls ./build/$branch/
+  cp -v -u -r $workdir/book/ $workdir/build/$branch/
+  ls $workdir/build/$branch/
 done
 
-ls ./build
+ls $workdir/build
 echo "构建完成"
--- scripts/replace_site_url.py
+++ scripts/replace_site_url.py
@@ -2,28 +2,15 @@
 
 import subprocess
 import sys
-
-def get_current_branch():
-    """获取当前 Git 分支名称"""
-    try:
-        result = subprocess.run(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], capture_output=True, text=True)
-        return result.stdout.strip()
-    except Exception as e:
-        print(f"获取当前分支名称时出错：{e}")
-        sys.exit(1)
-
 def replace_site_url(file_path, branch_name):
     """替换文件中的 {site.url} 为当前分支名称"""
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, 'r', encoding='utf-8') as file:
             content = file.read()
-
-        # if(branch_name == 'main'):
-        #     branch_name = 'latest'
         
         new_content = content.replace('{site.url}', branch_name)
         
-        with open(file_path, 'w') as file:
+        with open(file_path, 'w', encoding='utf-8') as file:
             file.write(new_content)
         
         print(f"用{branch_name} in {file_path}" + "替换{site.url}。")
@@ -34,11 +21,14 @@ def replace_site_url(file_path, branch_name):
 if __name__ == "__main__":
     """
     将指定文件中的 {site.url} 替换为当前分支名称。
+
+    :param file_path: 欲替换的源文件路径
+    :param branch_name: 目标文件路径
     """
-    if len(sys.argv) != 2:
-        print("使用方法：python replace_site_url.py <文件>")
+    if len(sys.argv) != 3:
+        print("使用方法：python replace_site_url.py <文件> <分支名称>")
         sys.exit(1)
     
     file_path = sys.argv[1]
-    branch_name = get_current_branch()
+    branch_name = sys.argv[2]
     replace_site_url(file_path, branch_name)
